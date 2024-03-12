068bc4634fea14aa73f7c5385cac47468e2833d3
dulminiguruge <dkguruge@gmail.com>
2024-03-12 17:10:24 -0600

    updated

--- cryptoclient/cryptoclient/settings/base.py
+++ cryptoclient/cryptoclient/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)=&5$*j92f%qf7@wx*jms5t6v^&21n=dt)j%c$0vjf7d-fokwx
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
